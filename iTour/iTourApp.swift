c706d6a31eb49f756aad5af4d48306ccdab4e8b4
Joel Lacerda <joellacerdaol@gmail.com>
2023-11-28 14:25:17 -0300

    Initial Commit

---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Joel Lacerda on 28/11/23.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
