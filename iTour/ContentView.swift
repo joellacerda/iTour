3ac58b86c79d41bde4ce650ad21459f024aa482a
Joel Lacerda <joellacerdaol@gmail.com>
2023-11-30 19:35:47 -0300

    initial commit

--- iTour/ContentView.swift
+++ iTour/ContentView.swift
@@ -6,16 +6,48 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query var destinations: [Destination]
+    
+    @State private var path = [Destination]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack(path: $path) {
+            List {
+                ForEach(destinations) { destination in
+                    NavigationLink(value: destination) {
+                        VStack(alignment: .leading) {
+                            Text(destination.name)
+                                .font(.headline)
+
+                            Text(destination.date.formatted(date: .long, time: .shortened))
+                        }
+                    }
+                }
+                .onDelete(perform: deleteDestinations)
+            }
+            .navigationTitle("iTour")
+            .navigationDestination(for: Destination.self, destination: EditDestinationView.init)
+            .toolbar {
+                Button("Add Destination", systemImage: "plus", action: addDestination)
+            }
         }
-        .padding()
+    }
+    
+    func addDestination() {
+        let destination = Destination()
+        modelContext.insert(destination)
+        path = [destination]
+    }
+    
+    func deleteDestinations(_ indexSet: IndexSet) {
+        for index in indexSet {
+            let destination = destinations[index]
+            modelContext.delete(destination)
+        }
     }
 }
 
---
+++ iTour/Destination.swift
@@ -0,0 +1,24 @@
+//
+//  Destination.swift
+//  iTour
+//
+//  Created by Joel Lacerda on 28/11/23.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Destination {
+    var name: String
+    var details: String
+    var date: Date
+    var priority: Int
+    
+    init(name: String = "", details: String = "", date: Date = .now, priority: Int = 2) {
+        self.name = name
+        self.details = details
+        self.date = date
+        self.priority = priority
+    }
+}
