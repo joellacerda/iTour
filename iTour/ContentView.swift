0d243b78afca0977adba69737d3984c86f3ea23d
Joel Lacerda <joellacerdaol@gmail.com>
2023-12-05 14:22:25 -0300

    add search bar to filter destinations

--- iTour/ContentView.swift
+++ iTour/ContentView.swift
@@ -12,12 +12,14 @@ struct ContentView: View {
     @Environment(\.modelContext) var modelContext
     @State private var path = [Destination]()
     @State private var sortOrder = SortDescriptor(\Destination.name)
+    @State private var searchText = ""
     
     var body: some View {
         NavigationStack(path: $path) {
-            DestinationListingView(sort: sortOrder)
+            DestinationListingView(sort: sortOrder, searchString: searchText)
                 .navigationTitle("iTour")
                 .navigationDestination(for: Destination.self, destination: EditDestinationView.init)
+                .searchable(text: $searchText)
                 .toolbar {
                     Button("Add Destination", systemImage: "plus", action: addDestination)
                     Menu("Sort", systemImage: "arrow.up.arrow.down") {
@@ -39,8 +41,6 @@ struct ContentView: View {
         modelContext.insert(destination)
         path = [destination]
     }
-    
-    
 }
 
 #Preview {
--- iTour/DestinationListingView.swift
+++ iTour/DestinationListingView.swift
@@ -28,8 +28,14 @@ struct DestinationListingView: View {
         }
     }
     
-    init(sort: SortDescriptor<Destination>) {
-        _destinations = Query(sort: [sort])
+    init(sort: SortDescriptor<Destination>, searchString: String) {
+        _destinations = Query(filter: #Predicate {
+            if searchString.isEmpty {
+                return true
+            } else {
+                return $0.name.localizedStandardContains(searchString)
+            }
+        }, sort: [sort])
     }
     
     func deleteDestinations(_ indexSet: IndexSet) {
@@ -41,5 +47,5 @@ struct DestinationListingView: View {
 }
 
 #Preview {
-    DestinationListingView(sort: SortDescriptor(\Destination.name))
+    DestinationListingView(sort: SortDescriptor(\Destination.name), searchString: "")
 }
