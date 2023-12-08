2111f866a1d8c8bea1b0f342b865a2f2b58d20e7
Joel Lacerda <joellacerdaol@gmail.com>
2023-12-08 10:33:28 -0300

    add Sight relationship

---
+++ iTour/Sight.swift
@@ -0,0 +1,18 @@
+//
+//  Sight.swift
+//  iTour
+//
+//  Created by Joel Lacerda on 08/12/23.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Sight {
+    var name: String
+
+    init(name: String) {
+        self.name = name
+    }
+}
